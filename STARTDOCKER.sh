3a33268f9c7e481aa2468713d7052ee79d6dd0a2
Fabio Santana <fabio@4comtec.com.br>
2021-12-01 16:31:43 -0300

    remove instruction start docker.

--- STARTDOCKER.sh
+++
@@ -1,4 +0,0 @@
-docker-compose up -d && docker-compose exec app bash -c "php artisan serve --port 8080 --host 0.0.0.0" &
-
-docker-compose exec -T db sh -c 'exec mysql -uroot -p"desafio123"' < dump.sql
-
