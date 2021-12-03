6e8219a809a4a14e1eb541e5460bd79f011d8438
Fabio Santana <fabio@4comtec.com.br>
2021-12-03 08:33:12 -0300

    adding whole patch for controllers

--- app/sistema/routes/api.php
+++ app/sistema/routes/api.php
@@ -3,11 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-Route::post('user/add', 'User\\UserController@store');
-Route::get('user/getTotalBalance', 'User\\UserController@getTotalBalance');
-Route::post('transaction/add', 'Transaction\\TransactionController@store');
+Route::post('user/add', 'App\Http\Controllers\User\UserController@store');
+Route::get('user/getTotalBalance', 'App\Http\Controllers\User\UserController@getTotalBalance');
+Route::post('transaction/add', 'App\Http\Controllers\Transaction\TransactionController@store');
 
